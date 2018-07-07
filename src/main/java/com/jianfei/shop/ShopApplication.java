cd9e438ae0df299356adae70bd565ac3e83fee51
jianfei.pang <jianfei.pang@qunar.com>
2018-07-08 02:32:58 +0800

    添加日志打印配置

--- src/main/java/com/jianfei/shop/ShopApplication.java
+++ src/main/java/com/jianfei/shop/ShopApplication.java
@@ -1,12 +1,22 @@
 package com.jianfei.shop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pangjianfei
+ * Date:2018-07-05
+ * spring-boot应用启动入口
+ */
 @SpringBootApplication
 public class ShopApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ShopApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("spring-boot,web商城开始启动");
 		SpringApplication.run(ShopApplication.class, args);
 	}
 }
